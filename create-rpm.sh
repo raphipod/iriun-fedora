e34e0055b3d9ca23c2017a4aa26f8ad4a8351712
Raphipod <56308821+raphipod@users.noreply.github.com>
2023-03-17 16:15:12 +0100

    Create create-rpm.sh

---
+++ create-rpm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check for dependencies: alien, v4l2loopback
+
+if ! command -v alien &> /dev/null
+then
+  echo "alien not found. Installing..."
+  sudo dnf install alien -y
+fi
+
+if ! v4l2loopback-ctl &> /dev/null
+then
+  echo "v4l2loopback not found. Installing..."
+  sudo dnf install v4l2loopback -y
+fi
+
+# Convert .deb package to .rpm using alien
+
+sudo alien -r iriunwebcam-*.deb --target=x86_64
+
+# Force-install iriunwebcam package with rpm (CHANGE UPON VERSION CHANGE!)
+
+sudo rpm -Uvh iriunwebcam-2.8-2.x86_64.rpm --force
